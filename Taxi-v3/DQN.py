6f52ec1aa42d90be045e0ce79fa34e01673fb8f9
N_Bharath_Chowdhary <n.bharath.chowdhary@gmail.com>
2024-04-23 13:59:27 +0200

    Added DQN class

--- Taxi-v3/DQN.py
+++ Taxi-v3/DQN.py
@@ -4,3 +4,14 @@ import gymnasium as gym
 import imageio
 from IPython.display import Image
 from gymnasium.utils import seeding
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DQN(nn.Module):
+      def __init__(self):
+          super(Model,self).__init__()
+
+      def forward():
+          pass
