9c71d0bc97698723963c290a423727d0e1bb53ce
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-05-26 18:19:36 +0200

    final commit

--- Taxi-v3/DQN.py
+++ Taxi-v3/DQN.py
@@ -36,8 +36,8 @@ class DQN():
         self.gamma = 0.99
         self.n_episodes = 1000
         self.epsilon = 1
-        self.mini_batch_size = 64
-        self.max_memory_size = 100000   #check mini_batch_size ratio over max_memory_size (check literature)
+        self.mini_batch_size = 4
+        self.max_memory_size = 100   #check mini_batch_size ratio over max_memory_size (check literature)
         self.replay_memory=[]
         self.model = nn_model(self.env)
         self.loss_fn = nn.MSELoss()
@@ -51,22 +51,27 @@ class DQN():
     def replay(self, replay_memory):
         
         minibatch = np.random.choice(replay_memory, self.mini_batch_size, replace=True)
+
+        #print(minibatch['state'])
+        #for i, sample in enumerate(minibatch):
+        #       print(f"Sample {i} state shape: {np.array(sample['state']).shape}")
+
         
-        state_list      =      np.array(list(map(lambda x: x['state'], minibatch)))
+        state_list      =      torch.from_numpy(np.array(list(map(lambda x: x['state'], minibatch))))
         action_list     =      np.array(list(map(lambda x: x['action'], minibatch)))
         reward_list     =      np.array(list(map(lambda x: x['reward'], minibatch)))
-        next_state_list =      np.array(list(map(lambda x: x['next_state'], minibatch)))
+        next_state_list =      torch.from_numpy(np.array(list(map(lambda x: x['next_state'], minibatch))))
         done_list       =      np.array(list(map(lambda x: x['done'], minibatch)))
 
-        q_values_next_state        =      self.model.predict(next_state_list)
+        q_values_next_state        =      self.model(next_state_list)
 
-        q_values_current_state     =      self.model.predict(state_list)
+        q_values_current_state     =      self.model(state_list)
 
-        q_values_update            =      self.model.predict(state_list)
+        q_values_update            =      self.model(state_list)
 
         for ite,(state,action,reward,q_values_next_state, done) in enumerate(zip(state_list,action_list,reward_list,q_values_next_state, done_list)): 
             if not done:  
-                target = reward + self.gamma * np.max(q_values_next_state)
+                target = reward + self.gamma * np.max(q_values_next_state.detach().numpy())
             else:
                 target = reward
 
@@ -83,28 +88,32 @@ class DQN():
     def train(self):
         env = self.env
         
-        episode_rewards = []
+        self.episode_rewards = []
         for episode in range(self.n_episodes):
             state = env.reset()
             done = False
             sum_reward = 0
-            self.len_state = len(state[0])
+            state= state[0]
+            self.len_state = len(state)
             while not done:
-                print("{}".format(state[0]))
-                q_action = self.model(torch.from_numpy(np.reshape(state[0],[1,self.len_state]))) #output from DQN model
+                #print("{}".format(state[0]))
+                q_action = self.model(torch.from_numpy(np.reshape(state,[1,self.len_state]))) #output from DQN model
                 
+                #print("-------------------------------------------------This has passed successfully---------------------------------------------")
+                #print(state)
                 #take action according to epsilon-greedy policy
                 if np.random.rand() < self.epsilon: 
                     action = env.action_space.sample()
                 else:
                     action = np.argmax(q_action.detach().numpy())
-                print(env.step(action))
+                #print(env.step(action))
                 # execute the action 
                 next_state, reward, done, info, _ = env.step(action)
                 
                 # add reward to the sum
                 sum_reward += reward
                 
+                #print("this is before: {} {}".format(state, next_state))
                 # add the transition to the replay memory
                 if len(self.replay_memory) < self.max_memory_size:
                     self.replay_memory.append({"state":state, "action":action, "reward":reward, "next_state":next_state, "done":done})
@@ -117,13 +126,14 @@ class DQN():
                 #update state
                 state = next_state                
                 
-                if epsilon > 0.01:
-                    epsilon -= 0.01
+                if self.epsilon > 0.01:
+                    self.epsilon -= 0.01
             
             print(f"Episode: {episode}, Reward: {sum_reward}")
-            episode_rewards.append(sum_reward) 
-
+            self.episode_rewards.append(sum_reward) 
+        with open("file.txt", "w") as output:
+             output.write(str(self.episode_rewards))
 
 
 if __name__ == "__main__":
-    DQN()
+    dqn = DQN()
